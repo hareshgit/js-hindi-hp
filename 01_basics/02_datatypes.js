f8941883845a06096da8ef30a5a0dcebbba92a34
hareshgit <hareshvaviya@gmail.com>
2024-08-30 04:46:13 +0000

    data types done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+// alert (3 +3)    // we are using nodejs, not browser.
+/* console.log(3
+    +
+    3)
+    */ //code readability should be high
+
+    console.log("Haresh");
+    
+    let name = "Haresh"
+    let age = 49
+    let IsLoggedIn = true
+
+    // Premitive data types
+    // number => 2 to power 53
+    // bigint =>
+    // string => ""
+    // boolean => true/false
+    // null => standalone value. representation of empty value. Value not available.
+    // undefined => variable defind without value. Value not assigned
+    // symbol => use for uniqueness
+
+    // obejct => 
+        
+        console.log(typeof age)         // number
+        console.log(typeof "Haresh")    // string
+        console.log(typeof true)        // boolean
+        console.log(typeof null)        // object
+        console.log(typeof undefined)   // undefined  
+        
+    
+    
