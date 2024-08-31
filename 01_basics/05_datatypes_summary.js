31d613097f845ef04b1ece5ae80324157195b61b
hareshgit <hareshvaviya@gmail.com>
2024-08-31 06:02:58 +0000

    Datatypes_Summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,50 @@
+// Primitive Data types (call by value)
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100               // Data type number
+const scoreValue = 100.3        // Data type number
+
+const isLoggedIn = true         // Data type boolean
+const outSideTemp = null        // Data type object
+let userEmail;                  // Data type undefined
+
+const id = Symbol('123')            // Data type symbol
+const anotherId = Symbol('123')     // Data type symbol
+
+console.log(id);                    //Output: Symbol(123)
+console.log(anotherId);             //Output: Symbol(123)
+console.log(id == anotherId);       //Output: false
+
+const bigNumber = 344545465464654645645645564563    // Data type number
+
+// Non Primitive Data type (Reference)
+// 3 Types: Array, Objects, Functions
+// data types for all is Function
+
+const heros = ["Shaktiman", "Nagraaj", "Doga"]      // Data type object
+let myObj = {
+    name: "Haresh",
+    Age: 23
+}                                                   // Data type object
+
+console.log(heros);
+console.log(myObj);
+
+const myFunction = function(){
+    console.log("Hello World!");
+}                                                   // Data type function
+
+myFunction();
+
+console.log("============Data types of above defined parameters================")
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outSideTemp);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof anotherId);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
